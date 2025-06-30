f5346b655a649cf044cb964563d4654dedd361a7
Angela Mihajlova <angeamih123@gmail.com>
2025-06-30 12:44:25 +0200

    Various changes and updates

--- accommodation-app-frontend/src/axios/axios.js
+++ accommodation-app-frontend/src/axios/axios.js
@@ -1,7 +1,23 @@
+// import axios from "axios";
+//
+// const axiosInstance = axios.create({
+//     baseURL: "http://localhost:9090/api",
+//     headers: {
+//         "Content-Type": "application/json",
+//     },
+// });
+//
+// export default axiosInstance;
+
 import axios from "axios";
 
+const baseURL =
+    import.meta.env.MODE === 'development'
+        ? 'http://localhost:9090/api'        // кога работиш со docker compose локално
+        : 'http://backend.local/api';         // кога работиш со Kubernetes Ingress
+
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:9090/api",
+    baseURL,
     headers: {
         "Content-Type": "application/json",
     },
